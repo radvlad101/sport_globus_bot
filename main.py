0995d455ff18df120e90e76318d1684db7232081
radvlad101 <radvlad101biz@gmail.com>
2025-08-23 03:36:11 +0300

    commit 60

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_latest_news():
         "q": "football OR soccer",
         "from": from_date,
         "to": to_date,
-        "language": "ru",
+        "language": "en",
         "sortBy": "popularity",
         "pageSize": 10,
         "apiKey": API_KEY
